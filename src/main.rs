7a89ed1579b3b8f3a9a40fd84db7a8ab92a02e77
cp-daniel-mccoy <86984714+cp-daniel-mccoy@users.noreply.github.com>
2021-08-14 09:47:58 -0400

    Added some error handling for user friendliness. Optimzed the program for public consumption. Changed the author public email.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-#[allow(unused_imports)]
 use std::io;
-#[allow(unused_imports)]
-use std::io::*;
-#[allow(unused_imports)]
 use std::io::{BufRead};
-#[allow(unused_imports)]
 use std::convert::TryInto;
-#[allow(unused_imports)]
 use rand::prelude::*;
-#[allow(unused_imports)]
 use rand::Rng;
 
 #[allow(non_snake_case)]
+#[quit::main]
 fn main() {
     askMe();
 }
@@ -21,21 +15,28 @@ fn space() {
 }
 
 #[allow(non_snake_case)]
-#[allow(unused_variables)]
 fn askMe() {
     space();
-    println!("This program will give you a list of your entered selections.");
-    println!("In this scenario, we are going to use foods.");
+    println!("This program will ask you for a list of options and then choose one for you.");
+    println!("Enter options like places to eat or things to cook.");
     space();
-    println!("Enter your favorite foods line by line, when you are done, just type 'done'");
+    println!("Enter each option line by line. When you are done, just type 'done'");
     space();
     let mut theList = Vec::new();
     let mut doneYet = false;
     let stdin = io::stdin();
     while doneYet == false {
         let toBeAdded = stdin.lock().lines().next().unwrap().unwrap();
+
         if toBeAdded.to_uppercase() == "DONE" {
             doneYet = true;
+        } else if toBeAdded == "" {
+            space();
+            println!("Error: You did not enter any input.");
+            println!("The program has crashed.");
+            println!("This is a known error and will be fixed in a later update.");
+            space();
+            quit::with_code(0);
         } else {
             space();
             println!("Your selection has been added to the list.");
@@ -44,8 +45,18 @@ fn askMe() {
         }
     }
 
-    let theRand:i32 = random();
+    let _theRand:i32 = random();
     let mut vecLength:i32 = theList.len().try_into().unwrap();
+
+    if vecLength == 0 {
+        space();
+        println!("Error: You did not enter any input.");
+        println!("The program has crashed.");
+        println!("This is a known error and will be handled in a future update.");
+        space();
+        quit::with_code(0);
+    }
+
     vecLength -= 1;
     let mut rng = rand::thread_rng();
     let selection:usize = rng.gen_range(0..vecLength).try_into().unwrap();
